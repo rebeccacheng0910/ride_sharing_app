63f8423d3e61ee60348c32c904b8990058005dfc
Rebecca Cheng <rebeccacheng910@hotmail.com>
2020-10-25 09:40:33 -0700

    Initial Add

---
+++ Uber_Clone/Controller/LoginController.swift
@@ -0,0 +1,23 @@
+//
+//  LoginController.swift
+//  Uber_Clone
+//
+//  Created by Xinyi Cheng on 25.10.20.
+//
+
+import UIKit
+
+class LoginController: UIViewController {
+    
+    // MARK: Properties
+    
+    
+    // MARK: LifeCycle
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
